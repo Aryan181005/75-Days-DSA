d7a1099462e7578539832733e02775c685c31e05
Aryan Singh <aryansingh@Aryans-MacBook-Air.local>
2025-12-07 22:39:39 +0530

    Day 14

---
+++ day_14/RainWater.java
@@ -0,0 +1,26 @@
+package day_14;
+
+class Solution {
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+
+        int maxL = height[left];
+        int maxR = height[right];
+
+        int water = 0;
+
+        while(left < right){
+            if(maxL <= maxR){
+                left++;
+                maxL = Math.max(maxL, height[left]);
+                water += maxL - height[left];
+            }else{
+                right--;
+                maxR = Math.max(maxR, height[right]);
+                water += maxR - height[right];
+            }
+        }
+        return water;
+    }
+}
